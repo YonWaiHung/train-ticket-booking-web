8b46a2bbfe87cd587ece90f282f92a766574be01
YonWaiHung <waihung.yon@gmail.com>
2024-10-27 01:19:16 +0800

    Initialization of some of admins components

---
+++ src/views/AdminDashboard.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="admin-dashboard">
+      <h1>Admin Dashboard</h1>
+      <button @click="signOut">Sign Out</button>
+      <section>
+        <h2>Manage Train Schedules</h2>
+        <TrainScheduleManager />
+      </section>
+      
+      <section>
+        <h2>Current Bookings</h2>
+        <BookingOverview />
+      </section>
+      
+      <section>
+        <h2>User Management</h2>
+        <UserManagement />
+      </section>
+    </div>
+  </template>
+
+
+<script>
+import { signOut } from 'firebase/auth'
+import { useUserStore } from '@/stores/userStore'
+import { computed } from 'vue'
+import TrainScheduleManager from '@/components/TrainScheduleManager.vue'
+import BookingOverview from '@/components/BookingOverview.vue'
+import UserManagement from '@/components/UserManagement.vue'
+
+export default {
+    name: "adminDashboard",
+    components: {
+        TrainScheduleManager,
+        BookingOverview,
+        UserManagement
+    },
+    setup() {
+        const userStore = useUserStore();
+        const adminName = computed(() => userStore.userName);
+        
+        return { adminName };
+    },
+    props: ['signOut'],
+}
+</script>
+
+<style scoped>
+.admin-dashboard {
+  padding: 1em;
+}
+</style>
